07fc8aee672e5a262307645891f482dafdf93eec
supaiku2452 <supaiku2452@gmail.com>
2021-02-06 23:40:32 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Shigenori Ozawa on 2021/02/06.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
